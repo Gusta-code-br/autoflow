1a9fc07bea2459c717e2a7e708ad7660cb10d626
Gusta-code-br <gustavohenrique92rv@gmail.com>
2026-08-03 02:07:20 -0300

    teste.

--- src/components/toast-host.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useApp } from "@/lib/store";
-import { Toasts } from "./ui";
-
-export function ToastHost() {
-  const { toasts, fecharToast } = useApp();
-  return <Toasts toasts={toasts} aoFechar={fecharToast} />;
-}
